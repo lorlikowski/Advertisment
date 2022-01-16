83ffb8178387387198eeba8a288abf7542ba1951
Adam Cichy <adamik32@outlook.com>
2022-01-16 21:01:25 +0100

    frontend: very basic things

---
+++ frontend/advertisments/src/components/AdvertisementsList.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div v-for="advertisement in advertisements" :key="advertisement.id">
+      <b-card-group deck>
+        <b-card :title="advertisement.title">
+          <b-card-text>
+          {{advertisement.description}}
+          </b-card-text>
+          <template #footer>
+            <span>
+              <div class="d-flex justify-content-between">
+                <div class="d-flex-row">
+                  <div>
+                    Wyświetlenia:
+                    <span class="text-primary">{{advertisement.views}}</span>
+                  </div>
+                  <div>
+                    <small class="text-muted">
+                        Początek: {{advertisement.date_start}}
+                    </small>
+                  </div>
+                  <div>
+                    <small class="text-muted">
+                        Koniec: {{advertisement.date_end}}
+                    </small>
+                  </div>
+                </div>
+                <div>
+                  <b-button variant="outline-danger">
+                    <b-icon icon="bell-fill"></b-icon>
+                  </b-button>
+                  <b-avatar :to="{name: 'User', params:{id: advertisement.owner}}"></b-avatar>
+                  {{advertisement.owner}}
+                </div>
+              </div>
+            </span>
+          </template>
+        </b-card>
+      </b-card-group>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    advertisements: Array
+  },
+  data() {
+    return {
+      msg: "Hello",
+    };
+  },
+});
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ frontend/advertisments/src/views/User.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <b-container fluid="md">
+      <!-- <AdvertisementsList :advertisements="advertisements"/> -->
+      Użytkownik {{id}} wita na swoim profilu!
+    </b-container>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+// import AdvertisementsList from '@/components/AdvertisementsList.vue';
+
+export default Vue.extend({
+  components: {
+  },
+  props: {
+      id: String
+  },
+  data() {
+    return {
+    }
+  }
+})
+</script>
