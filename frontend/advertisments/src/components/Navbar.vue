6ad2da9bafea6cf282a238ea36e8c92ee8581142
Adam Cichy <adamik32@outlook.com>
2022-02-12 12:29:04 +0100

    navbar changes

--- frontend/advertisments/src/components/Navbar.vue
+++ frontend/advertisments/src/components/Navbar.vue
@@ -1,10 +1,20 @@
 <template>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+  <b-navbar sticky variant="light">
+    <b-navbar-nav>
+      <b-nav-item :to="{name: 'Home'}">Ogłoszenia</b-nav-item>
+      <b-nav-item :to="{name: 'Create'}">Dodaj ogłoszenie</b-nav-item>
+    </b-navbar-nav>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item :to="{name: 'Login'}">Zaloguj się</b-nav-item>
+      <b-nav-item :to="{name: 'User', params: {id: '1'}}">Mój profil</b-nav-item>
+    </b-navbar-nav>
+  </b-navbar>
+    <!-- <div id="nav">
+      <router-link to="/">Ogłoszenia</router-link> |
       <router-link to="/users/1">Mój profil</router-link>
       <router-link to="/login"> Zaloguj się</router-link>
       <router-link to="/"> Wyloguj </router-link>
-    </div>
+    </div> -->
 </template>
 
 <style lang="scss">
